9145a17f0415d2691694f9459a811462ed42176f
Christian Burkert <burkert@informatik.uni-hamburg.de>
2019-01-30 15:33:45 +0100

    Detailed setup.py infos

--- setup.py
+++ setup.py
@@ -4,18 +4,17 @@ from setuptools import setup, find_packages
 with open('README.md') as f:
     README = f.read()
 
-with open('LICENSE') as f:
-    LICENSE = f.read()
-
 setup(
     name='gitprivacy',
     version='1.1.0',
-    description='Git extension that adds timestamp blurring',
+    description='Git wrapper redacting author and committer dates.',
     long_description=README,
+    long_description_content_type="text/markdown",
+    keywords=["git", "privacy", "timestamps"],
     maintainer='Christian Burkert',
-    author_email='gitprivacy@cburkert.de',
+    maintainer_email='gitprivacy@cburkert.de',
     url='https://github.com/cburkert/pyGitPrivacy',
-    license=LICENSE,
+    license="BSD",
     packages=find_packages(exclude=('tests', 'docs')),
     include_package_data=True,
     python_requires='>=3.5',
@@ -29,5 +28,15 @@ setup(
         'console_scripts': [
             'git-privacy = gitprivacy.gitprivacy:main'
         ]
-    }
+    },
+    classifiers=[
+        "Topic :: Software Development :: Version Control :: Git",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
 )
