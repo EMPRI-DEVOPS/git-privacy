841853cb5ee7dc34cb293743ff4e744e7328cb04
Christian Burkert <burkert@informatik.uni-hamburg.de>
2019-01-30 13:59:55 +0100

    Set minimum python version

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setup(
     license=LICENSE,
     packages=find_packages(exclude=('tests', 'docs')),
     include_package_data=True,
+    python_requires='>=3.5',
     install_requires=[
         'gitpython',
         'colorama',
