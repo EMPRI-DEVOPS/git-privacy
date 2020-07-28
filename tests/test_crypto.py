41d060ec114b5723a76c34dd2cceaaa6a3ad4c07
Christian Burkert <burkert@informatik.uni-hamburg.de>
2020-07-28 10:00:00 +0200

    Extend crypto tests for key file based crypto

--- tests/test_crypto.py
+++ tests/test_crypto.py
@@ -1,7 +1,8 @@
 import unittest
 from nacl.exceptions import CryptoError
 
-from gitprivacy.crypto import PasswordSecretBox
+from gitprivacy.crypto import (PasswordSecretBox, SecretBox, MultiSecretBox,
+                               MultiSecretDecryptor)
 
 
 class CryptoTestCase(unittest.TestCase):
@@ -10,6 +11,11 @@ class CryptoTestCase(unittest.TestCase):
         c = PasswordSecretBox(salt, "passw0rd")
         enc = c.encrypt("foobar")
         self.assertEqual(c.decrypt(enc), "foobar")
+        # test keyfile interop
+        mbox = MultiSecretBox(key=c._export_key(), keyarchive=[])
+        self.assertEqual(mbox.decrypt(enc), "foobar")
+        enc2 = mbox.encrypt("foobar")
+        self.assertEqual(mbox.decrypt(enc2), "foobar")
 
     def test_wrongpwd(self):
         salt = PasswordSecretBox.generate_salt()
@@ -17,3 +23,18 @@ class CryptoTestCase(unittest.TestCase):
         c2 = PasswordSecretBox(salt, "password")
         enc = c.encrypt("foobar")
         self.assertEqual(c2.decrypt(enc), None)
+
+    def test_keyarchive(self):
+        key = SecretBox.generate_key()
+        box = SecretBox(key=key)
+        enc = box.encrypt("hello")
+        self.assertEqual(box.decrypt(enc), "hello")
+        mdec = MultiSecretDecryptor(keyarchive=[key])
+        self.assertEqual(mdec.decrypt(enc), "hello")
+        # different key in archive
+        key2 = SecretBox.generate_key()
+        mdec2 = MultiSecretDecryptor(keyarchive=[key2])
+        self.assertEqual(mdec2.decrypt(enc), None)
+        # both keys in archive
+        mdec3 = MultiSecretDecryptor(keyarchive=[key2, key])
+        self.assertEqual(mdec3.decrypt(enc), "hello")
