6e21fcfa5d3cd044a5d3d0cf76745c0a77fecef3
Christian Burkert <burkert@informatik.uni-hamburg.de>
2020-08-10 09:00:00 +0200

    Extend testing to more Python versions and include 3.8

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ setup(
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
     ],
