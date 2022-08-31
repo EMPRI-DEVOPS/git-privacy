1c0014d2176ac420d889a399942774fac631fdc6
Christian Burkert <burkert@informatik.uni-hamburg.de>
2022-09-01 00:00:00 +0200

    Prepare version 2.3.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as f:
 
 setup(
     name='gitprivacy',
-    version='2.2.0',
+    version='2.3.0',
     description='Git wrapper redacting author and committer dates.',
     long_description=README,
     long_description_content_type="text/markdown",
